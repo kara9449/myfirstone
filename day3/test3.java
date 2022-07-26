ecc10fc5514b278dfa8ca8d1ef5aae0b33bf9a0e
kara9449 <110051683+kara9449@users.noreply.github.com>
2022-07-26 16:34:27 -0400

    Update test3.java

--- day3/test3.java
+++ day3/test3.java
@@ -3,6 +3,10 @@ package day3;
 public class test3 {
     public static void main(String[] args) {
         System.out.println("kljkgekljhtrerth");
+        
+        //hghjkljhghj.  
+        
+      
     }
 
 
